54fda0f0d4857e3bea4bfd88de1b1e30cc0c1447
Mert <mertozata009@gmail.com>
2022-12-02 20:45:01 +0300

    SignLanguageQuestionGame

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public Question[] questions;
     private static List<Question> unansweredQuestions;
     private Question currentQuestion;
+    [SerializeField] private Text factText;
+    [SerializeField] private float timeBetweenQuestions = 1f;
+    [SerializeField] private Text trueAnswerText;
+    [SerializeField] private Text falseAnswerText;
+    [SerializeField] Animator animator;
+
     private void Start()
     {
         if (unansweredQuestions == null || unansweredQuestions.Count == 0)
@@ -24,8 +32,59 @@ public class GameManager : MonoBehaviour
         {
             int randomQuestionIndex = Random.Range(0, unansweredQuestions.Count);
             currentQuestion = unansweredQuestions[randomQuestionIndex];
-
-            unansweredQuestions.RemoveAt(randomQuestionIndex);
         }
+        SetRandomQuestion();
+    }
+
+    void SetRandomQuestion()
+    {
+        factText.text = currentQuestion.fact;
+
+        if (currentQuestion.isTrue)
+        {
+            trueAnswerText.text = "CORRECT";
+            falseAnswerText.text = "WRONG";
+        }
+        else
+        {
+            trueAnswerText.text = "WRONG";
+            falseAnswerText.text = "CORRECT";
+        }
+    }
+
+    IEnumerator TransitionToNextQuestion()
+    {
+        unansweredQuestions.Remove(currentQuestion);
+        yield return new WaitForSeconds(timeBetweenQuestions);
+        GetRandomQuestion();
+    }
+
+    public void SelectTrueButton()
+    {
+        animator.SetTrigger("True");
+        if (currentQuestion.isTrue)
+        {
+            Debug.Log("Correct");
+            
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+        StartCoroutine(TransitionToNextQuestion());
+    }
+    public void SelectFalseButton()
+    {
+        animator.SetTrigger("False");
+        if (!currentQuestion.isTrue)
+        {
+            Debug.Log("Correct");
+
+        }
+        else
+        {
+            Debug.Log("Wrong");
+        }
+        StartCoroutine(TransitionToNextQuestion());
     }
 }
